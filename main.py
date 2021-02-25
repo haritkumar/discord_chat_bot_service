05277a005937a66effbc0886b413c42f5f94cfd3
Kumar, Harit <harit_kumar@optum.com>
2021-02-25 12:02:03 +0530

    other files

---
+++ main.py
@@ -0,0 +1,19 @@
+import discord
+
+client = discord.Client()
+
+#on_ready() event is called when the bot is ready to start being used
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+#when the bot receives a message, the on_message() event is called
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('hi'):
+        await message.channel.send('hey')
+
+client.run('ODE0MzczMjk0MjkwNjMyNzE0.YDc6Ow.pTe9-XwWCDyUbgwA1sy6ZB8g0dM')
---
+++ readme.md
@@ -0,0 +1,12 @@
+# discord chat bot service
+This service recives messeges sent on channels and process them as bot reponse.
+
+## Install discord lib
+```sh
+python3 -m pip install -U discord.py
+```
+
+## Run program
+```sh
+python3 main.py
+```
